7e8cddf4f90fd346422494061f74dce2bd69aed2
apowers131 <arthurpow131@gmail.com>
2024-02-19 19:43:28 -0500

    player can infinitely jump now

--- SlugCatpanion/SlugCatpanion/SlugCatpanionMain.cs
+++ SlugCatpanion/SlugCatpanion/SlugCatpanionMain.cs
@@ -31,6 +31,24 @@ namespace SlugCatpanion
 
         private void OnEnable()
         {
+            On.Player.Update += PlayerUpdateHook;
+        }
+
+        void PlayerUpdateHook(On.Player.orig_Update orig, Player self, bool eu)
+        {
+            // Whenever Player.Update gets called by the game, it takes a detour into your code here instead.
+            // Do anything that you need to happen when the player updates in here.
+
+            orig(self, eu); // Then, use this to to tell the game that it needs to run the normal code (and other mods' hooks) now.
+
+            self.jumpBoost = 5;
+
+            //self.AddFood(1);
+            // And optionally, you can have more code here too, after orig
+            // In general, you will want to always try to call orig
+            // If you skip calling orig, this prevents all other mods from running their own hooks
+            // and it also stops the game from doing the vanilla behavior.
+
         }
     }
 }
